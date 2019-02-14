5f965912f177a1cab1d237db549f838c565f091d
Philipp M. Scholl <pscholl@ese.uni-freiburg.de>
2019-02-14 16:51:37 +0100

    rebuild for continious running

--- bgrecoder/src/main/java/de/uni_freiburg/bgrecorder/RecorderService.java
+++ bgrecoder/src/main/java/de/uni_freiburg/bgrecorder/RecorderService.java
@@ -4,6 +4,7 @@ import android.app.Notification;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -53,6 +54,7 @@ public class RecorderService extends Service {
     /* for start synchronization */
     private Long mStartTimeNS = -1l;
     private CountDownLatch mSyncLatch = null;
+    private MainReceiver mMainReceiver = null;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -84,128 +86,127 @@ public class RecorderService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
 
-        String platform = Build.BOARD + " " + Build.DEVICE + " " + Build.VERSION.SDK_INT,
-               output = getDefaultOutputPath(getApplicationContext()),
-               android_id =  Settings.Secure.getString(
-                               getContentResolver(), Settings.Secure.ANDROID_ID),
-               format = ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN ? "f32le" : "f32be",
-               action = intent.getAction();
-
-        if (ACTION_STOP.equals(action)) {
-            onDestroy();
-            return START_NOT_STICKY;
-        }
-
-        if (mFFmpeg != null)
-            return START_STICKY;
-
-        /**
-         *  Get the sensors from the system and check if they are wakeups
-         */
-        SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
-        Sensor[] sensors = new Sensor[] {
-            sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER, true),
-            sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD, true),
-            sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE, true),
-            sm.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR, true)
-        };
-        CopyListener[] listeners = new CopyListener[sensors.length];
-
-        for (Sensor s : sensors)
-            if (s==null || !s.isWakeUpSensor())
-                throw new Error("wakeup sensor not supported!");
-
-
-        /**
-         * build and start the ffmpeg process, which transcodes into a matroska file.
-         */
-        try {
-            FFMpegProcess.Builder b = new FFMpegProcess.Builder(getApplicationContext())
-                    .setOutput(output, "matroska")
-                    .setCodec("a", "wavpack")
-                    .addOutputArgument("-shortest")
-                    .setTag("recorder", "automotion " + VERSION)
-                    .setTag("android_id", android_id)
-                    .setTag("platform", platform)
-                    .setTag("fingerprint", Build.FINGERPRINT)
-                    .setTag("beginning", getCurrentDateAsIso());
-
-            for (Sensor s : sensors)
-                b
-                        .addAudio(format, RATE, getNumChannels(s))
-                        .setStreamTag("name", s.getName());
-
-
-            mFFmpeg = b.build();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return START_NOT_STICKY;
-        }
-
-        /**
-         * for each sensor there is thread that copies data to the ffmpeg process. For startup
-         * synchronization the threads are blocked until the starttime has been set at which
-         * point the threadlock will be released.
-         */
-        int us = (int) (1e6/RATE);
-
-        for (int i=0; i < sensors.length; i++) {
-            Sensor s = sensors[i];
-            HandlerThread t = new HandlerThread(s.getName()); t.start();
-            Handler h = new Handler(t.getLooper());
-            CopyListener l = new CopyListener(i, RATE, s.getName());
-            sm.registerListener(l, s, us, s.getFifoMaxEventCount()/2 * us, h);
-            mSensorListeners.add(l);
-        }
-
-        SyncLockListener lock = new SyncLockListener(sensors);
-        for (int i=0; i < sensors.length; i++)
-            sm.registerListener(lock, sensors[i], us);
-
-
-        /** notify the system that a new recording was started, and make
-         * sure that the service does not get called when an activity is
-         * destroyed by using the startForeground method. If not doing so,
-         * the service is also killed when an accompanying Activity is
-         * destroyed (wtf). */
+        /** We continiously start the service to receive Battery Events, which can not be done
+         * differently under Android 8.0 since no Battery PLUG/UNPLUG events are sent to
+         * broadcast receivers anymore. */
         Notification notification = new Notification.Builder(this)
                 .setSmallIcon(R.drawable.ic_notification)
                 .setContentTitle(getString(R.string.notification_title))
                 .setContentText(getString(R.string.notification_text))
                 .setPriority(Notification.PRIORITY_LOW)
                 .build();
+
         startForeground(NOTIFICATION_ID, notification);
 
-        return START_NOT_STICKY;
+        if (mMainReceiver == null) {
+            mMainReceiver = new MainReceiver();
+
+            IntentFilter power = new IntentFilter();
+            power.addAction(Intent.ACTION_POWER_CONNECTED);
+            power.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+            registerReceiver(mMainReceiver, power);
+        }
+
+        /**
+         * start the recording process if there is no ffmpeg instance yet, and no stop intent
+         * was sent.
+         */
+        if (ACTION_STOP.equals(intent.getAction()))
+            stopRecording();
+
+        else if (mFFmpeg == null)
+            try {
+                startRecording();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
+        stopRecording();
+    }
+
+    public void startRecording() throws Exception {
+        String platform = Build.BOARD + " " + Build.DEVICE + " " + Build.VERSION.SDK_INT,
+                output = getDefaultOutputPath(getApplicationContext()),
+                android_id = Settings.Secure.getString(
+                        getContentResolver(), Settings.Secure.ANDROID_ID),
+                format = ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN ? "f32le" : "f32be";
+
+        /**
+         *  Get the sensors from the system and check if they are wakeups
+         */
+        SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor[] sensors = new Sensor[]{
+                /*    sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER, true),
+                    sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD, true),
+                    sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE, true),
+                */
+                sm.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR, true)
+        };
+        CopyListener[] listeners = new CopyListener[sensors.length];
+
+        for (Sensor s : sensors)
+            if (s == null || !s.isWakeUpSensor())
+                throw new Error("wakeup sensor not supported!");
+
+
+        /**
+         * build and start the ffmpeg process, which transcodes into a matroska file.
+         */
+        FFMpegProcess.Builder b = new FFMpegProcess.Builder(getApplicationContext())
+                .setOutput(output, "matroska")
+                .setCodec("a", "wavpack")
+                .addOutputArgument("-shortest")
+                .setTag("recorder", "automotion " + VERSION)
+                .setTag("android_id", android_id)
+                .setTag("platform", platform)
+                .setTag("fingerprint", Build.FINGERPRINT)
+                .setTag("beginning", getCurrentDateAsIso());
+
+        for (Sensor s : sensors)
+            b
+            .addAudio(format, RATE, getNumChannels(s))
+            .setStreamTag("name", s.getName());
+
+
+        mFFmpeg = b.build();
+
+
+        /**
+         * for each sensor there is thread that copies data to the ffmpeg process. For startup
+         * synchronization the threads are blocked until the starttime has been set at which
+         * point the threadlock will be released.
+         */
+        int us = (int) (1e6 / RATE);
+
+        for (int i = 0; i < sensors.length; i++) {
+            Sensor s = sensors[i];
+            HandlerThread t = new HandlerThread(s.getName());
+            t.start();
+            Handler h = new Handler(t.getLooper());
+            CopyListener l = new CopyListener(i, RATE, s.getName());
+            sm.registerListener(l, s, us, s.getFifoMaxEventCount() / 2 * us, h);
+            mSensorListeners.add(l);
+        }
+
+        SyncLockListener lock = new SyncLockListener(sensors);
+        for (int i = 0; i < sensors.length; i++)
+            sm.registerListener(lock, sensors[i], us);
+    }
+
+
+    public void stopRecording() {
         if (mFFmpeg != null) {
             try {
                 SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
 
                 for (CopyListener l : mSensorListeners)
                     sm.flush(l);
-
-                /** call onFlushCompleted, in case the system's onFlushCompleted does not work,
-                 * which would lead to the whole system waiting forever. For this also a new
-                 * sensorevent needs to be posted to make sure that the copylistener exits.
-                Runnable timeout = new Runnable() {
-                    @Override
-                    public void run() {
-                        for (CopyListener l : mSensorListeners) {
-                            Log.e("bgrec", "flush timed out");
-                            l.onFlushCompleted(null);
-                            l.onSensorChanged(null);
-                        }
-                    }
-                };
-
-                HandlerThread th = new HandlerThread("flush timeout"); th.start();
-                Handler h = new Handler(th.getLooper());
-                h.postDelayed(timeout, 10 * 1000);
-                */
 
                 mFFmpeg.waitFor();
             } catch (InterruptedException e) {
@@ -214,7 +215,6 @@ public class RecorderService extends Service {
         }
 
         mFFmpeg = null;
-        stopForeground(true);
         super.onDestroy();
     }
 
